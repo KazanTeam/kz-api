36a52b8c433302778ebfb285fdd13945f0e5acd8
Hung Luyen <hunglmse@gmail.com>
2018-08-01 01:52:44 +0700

    0.9.1 - add user/get

--- src/main/java/com/kazan/controller/KazanUserController.java
+++ src/main/java/com/kazan/controller/KazanUserController.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kazan.component.TelegramSendGroups;
 import com.kazan.model.KazanUser;
 import com.kazan.repository.GroupRepository;
@@ -60,6 +64,27 @@ public class KazanUserController {
 			} else {
 				userRepository.update(kazanUser);
 				return new ResponseEntity<String>("User edited successfully!", HttpStatus.ACCEPTED);
+			}
+		}
+	}
+
+	// New API, 3rd of user API
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.POST, path = "/user/get/{userId}")
+	public @ResponseBody ResponseEntity<String> getUser(@PathVariable("userId") int userId) {
+		if (userId < 1) {
+			return new ResponseEntity<String>("Invalid userId!!!", HttpStatus.UNAUTHORIZED);
+		} else {
+			KazanUser kazanUser = userRepository.getById(userId);
+			if (null == kazanUser) {
+				return new ResponseEntity<String>("No such user!!!", HttpStatus.UNAUTHORIZED);
+			} else {
+				try {
+					return new ResponseEntity<String>(new ObjectMapper().writeValueAsString(kazanUser), HttpStatus.ACCEPTED);
+				} catch (JsonProcessingException e) {
+					System.out.println("KazanController.getUser:" + e);
+					return new ResponseEntity<String>("Error parsing object!", HttpStatus.UNAUTHORIZED);
+				}
 			}
 		}
 	}
